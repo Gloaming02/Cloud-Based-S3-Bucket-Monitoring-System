86f2ca65bb1e4baddc3024bdb6fd3bbedff7eabc
Zhengyan Hu <91642985+Gloaming02@users.noreply.github.com>
2024-12-06 13:08:02 -0800

    d
    d

--- lambda/driver-lambda.py
+++ lambda/driver-lambda.py
@@ -11,13 +11,13 @@ def lambda_handler(event, context):
     s3_client.put_object(Bucket=bucket_name, Key='assignment1.txt', Body='Empty Assignment 1')
     time.sleep(2.5)  
     
-    s3_client.put_object(Bucket=bucket_name, Key='assignment1.txt', Body='Empty Assignment 2222222222')
+    s3_client.put_object(Bucket=bucket_name, Key='assignment2.txt', Body='Empty Assignment 2222222222')
     time.sleep(2.5)  
     
-    s3_client.delete_object(Bucket=bucket_name, Key='assignment1.txt')
-    time.sleep(2.5)  
+    # s3_client.delete_object(Bucket=bucket_name, Key='assignment1.txt')
+    # time.sleep(2.5)  
     
-    s3_client.put_object(Bucket=bucket_name, Key='assignment2.txt', Body='33')
+    s3_client.put_object(Bucket=bucket_name, Key='assignment3.txt', Body='33')
     time.sleep(2.5)
     
 #    api_url = "https://b8l5t6qtph.execute-api.us-east-1.amazonaws.com/test"
